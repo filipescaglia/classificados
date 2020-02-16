78db4d247b8d8602f57add7d04ea0515f4d6c8ad
Filipe Scaglia <filipecscaglia@gmail.com>
2020-02-15 21:27:51 -0300

    Database added

--- classificados.sql
+++ classificados.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15-Fev-2020 às 21:24
+-- Tempo de geração: 16-Fev-2020 às 01:27
 -- Versão do servidor: 10.4.10-MariaDB
 -- versão do PHP: 7.3.12
 
@@ -43,10 +43,12 @@ CREATE TABLE `anuncios` (
 --
 
 INSERT INTO `anuncios` (`id`, `id_usuario`, `id_categoria`, `titulo`, `descricao`, `valor`, `estado`) VALUES
-(3, 1, 1, 'Hublot', 'top', 250, 2),
-(4, 1, 2, 'teste editar', 'teste', 0, 2),
-(5, 1, 4, 'carro', 'dada', 0, 0),
-(6, 1, 2, 'rorpa', 'asd', 0, 0);
+(10, 1, 1, 'Hublot', 'Relógio', 123, 1),
+(11, 1, 2, 'Casaco', 'Casaco', 95, 1),
+(12, 1, 2, 'Casaco 2', 'Casaco 2', 99, 2),
+(13, 1, 2, 'Casaco 3', 'Desc', 100, 0),
+(14, 1, 4, 'Carro 1', 'Carro', 200, 0),
+(15, 1, 3, 'Computador', 'PC', 499, 0);
 
 -- --------------------------------------------------------
 
@@ -65,8 +67,13 @@ CREATE TABLE `anuncios_imagens` (
 --
 
 INSERT INTO `anuncios_imagens` (`id`, `id_anuncio`, `url`) VALUES
-(5, 3, '9c4a33265e96fa3c9377bff7647fdbd2.jpg'),
-(6, 3, 'c06f3613307302eec89967e17d9a4e32.jpg');
+(10, 11, '3cb425be0aefce1b6fa67c7931622447.jpg'),
+(11, 11, 'fcfb659090cf0291cf99130cdc95ecf2.jpg'),
+(12, 11, '1ca0219f5061e25e9b4e8558f5da6656.jpg'),
+(13, 12, '82c7910a6e90007d3113b1b9a869956f.jpg'),
+(14, 13, '0e3bb521ca4055fb35435f043e1d14cd.jpg'),
+(15, 10, '2ff3aae4ed6cee585e0e0201f5d0870a.jpg'),
+(16, 10, 'a35dec4f8913d97b328374a5ecff9c8e.jpg');
 
 -- --------------------------------------------------------
 
@@ -108,7 +115,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `telefone`) VALUES
-(1, 'Filipe', 'email@teste.com.br', '202cb962ac59075b964b07152d234b70', '123456789');
+(1, 'Filipe', 'email@teste.com.br', '202cb962ac59075b964b07152d234b70', '123456789'),
+(4, 'Usuario 2', 'user2@mail.com', '202cb962ac59075b964b07152d234b70', '994112341');
 
 --
 -- Índices para tabelas despejadas
@@ -146,13 +154,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `anuncios`
 --
 ALTER TABLE `anuncios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `anuncios_imagens`
 --
 ALTER TABLE `anuncios_imagens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `categorias`
@@ -164,7 +172,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
